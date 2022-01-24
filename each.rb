1840e42e4459305121506c856db7bcd2023ff8d2
Amy Holt <amy.elizabeth.holt@gmail.com>
2022-01-24 14:07:02 -0700

    Add reminder to run code

--- each.rb
+++ each.rb
@@ -1,3 +1,7 @@
+# As you complete each challenge, RUN THE CODE to ensure it works. You can do this by:
+  # a. Copy and pasting it into IRB
+  # b. Running the file in your Terminal. You can do this by running `ruby <filepath>`. If you are in this directory, you'd run `ruby each.rb`.
+  
 # Challenge 1
 # Use the each method to print out a complete sentence stating the weather using the Array below:
 weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
