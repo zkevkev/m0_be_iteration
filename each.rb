c1dabb045d05b7349dbd1410c83f82680dbc4236
Amy Holt <amy.elizabeth.holt@gmail.com>
2022-01-24 14:04:52 -0700

    Initial commit

---
+++ each.rb
@@ -0,0 +1,9 @@
+# Challenge 1
+# Use the each method to print out a complete sentence stating the weather using the Array below:
+weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
+
+# Challenge 2
+# Use the each method to print out the words that are exactly 3 characters in length
+words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
+
+# Record yourself live-coding and talking through either Challenge 1 or 2. The recording doesn't have to be the first time you are solving the problem. Use technical vocabulary and explain each part to demonstrate your understanding!
