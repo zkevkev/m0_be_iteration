ab2dc8fee6858590e58d3792088b569b861f6d83
Amy Holt <amy.elizabeth.holt@gmail.com>
2022-03-04 07:02:58 -0700

    Update each.rb

--- each.rb
+++ each.rb
@@ -10,4 +10,4 @@ weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
 # Use the each method to print out the words that are exactly 3 characters in length
 words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
 
-# Record yourself live-coding and talking through either Challenge 1 or 2. The recording doesn't have to be the first time you are solving the problem. Use technical vocabulary and explain each part to demonstrate your understanding!
+# Record yourself live-coding and talking through either Challenge 1 or 2. The recording doesn't have to be the first time you are solving the problem. Use technical vocabulary and explain each part to demonstrate your understanding! Share the link to your video in your small group channel.
