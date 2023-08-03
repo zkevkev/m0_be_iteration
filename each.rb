7782e8e43c58cc727a80d059b954d46a71e05ad4
Kevin Zolman <kevin.zolman@gmail.com>
2023-08-03 12:31:27 -0600

    Add weather status and word length to each.rb

--- each.rb
+++ each.rb
@@ -6,9 +6,19 @@
 # Use the each method to print out a complete sentence stating the weather using the Array below:
 weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
 
+weather_options.each do |weather|
+  puts "It's #{weather} out!"
+end
+
 # Challenge 2
 # Use the each method to print out the words that are exactly 3 characters in length
 words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
 
+words.each do |word|
+if word.length == 3
+    puts "#{word}"
+else
+end
+end
 
 # Record yourself live-coding and talking through either Challenge 1 or 2. The recording doesn't have to be the first time you are solving the problem. Use technical vocabulary and explain each part to demonstrate your understanding! Share the link to your video in your small group channel.
